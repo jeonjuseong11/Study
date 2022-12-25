20fb26499f8927965b88fdb05cbf17bd8125d4cf
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2022-12-25 13:25:38 +0900

    모던자바스크립트 프로그래밍의 정석 섹션4
    섹션4 마무리문제

---
+++ section4/commonDivisor.js
@@ -0,0 +1,14 @@
+function getGCD(n, m) {
+  //최대공약수는 영어로 greatest commmon divisor
+  let max = n > m ? n : m;
+  let GCD = 0;
+  for (let i = 1; i <= max; i++) {
+    if (n % i === 0 && m % i === 0) {
+      GCD = i; // 최대공약수
+    }
+  }
+  return GCD;
+}
+
+console.log(`308과 20의 최대공약수 : ${getGCD(308, 20)}`);
+console.log(`45과 38의 최대공약수 : ${getGCD(45, 38)}`);

b22e83c68f0c4def70f8bc3fc4d32a0d10a27e8b
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-01-18 15:15:51 +0900

    한 입 크기로 잘라먹는 리액트
    3-1

---
+++ 한입React/Truthy&Falsy.js
@@ -0,0 +1,15 @@
+//let a = "";   false로 인식
+//let a = null; false
+//let a = NaN; false
+//let a false
+//let a = undefined;//false로 인식
+//let a = "string"; //true로 인식
+//let a = [];//true로 인식
+//let a = {};//true
+//let a = Infinity //true
+
+if (a) {
+  console.log("TRUE");
+} else {
+  console.log("FALSE");
+}

6e3a56c84fef3a4a3a514500f3e8a9e392916f25
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-01-19 15:18:22 +0900

    React pra1
    React 컴포넌트 생성 및 props 주고받기 연습

---
+++ React.js/reactpra/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Todo from "./Todo";
+import { useState } from "react";
+function App() {
+  const [item, setItem] = useState({
+    id: "0",
+    title: "Hello World 1",
+    done: true,
+  });
+  return (
+    <div className="App">
+      <Todo item={item} />
+    </div>
+  );
+}
+
+export default App;

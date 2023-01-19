3226be105e45e8339cfcbd4953740fbc9766e86c
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-01-19 15:25:43 +0900

    React pra1-1
    배열 요소 추가
    배열 요소를 읽어 화면에 출력해주는 기능 추가

--- React.js/reactpra/src/App.js
+++ React.js/reactpra/src/App.js
@@ -3,16 +3,21 @@ import "./App.css";
 import Todo from "./Todo";
 import { useState } from "react";
 function App() {
-  const [item, setItem] = useState({
-    id: "0",
-    title: "Hello World 1",
-    done: true,
-  });
-  return (
-    <div className="App">
-      <Todo item={item} />
-    </div>
-  );
+  const [items, setItems] = useState([
+    {
+      id: "0",
+      title: "Hello World 1",
+      done: true,
+    },
+    {
+      id: "1",
+      title: "Hello World 2",
+      done: true,
+    },
+  ]);
+  let todoItems =
+    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
+  return <div className="App">{todoItems}</div>;
 }
 
 export default App;

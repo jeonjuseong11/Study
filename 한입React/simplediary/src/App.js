9b878701def398aa57055bf225b218ba93942369
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-02-11 15:55:36 +0900

    한 입 크기로 잘라먹는 리액트
    6-9. React에서 API 호출하기

--- 한입React/simplediary/src/App.js
+++ 한입React/simplediary/src/App.js
@@ -1,8 +1,10 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 //import Lifecycle from "./Lifecycle";
+//API 주소
+//https://jsonplaceholder.typicode.com/comments
 
 // const dummyList = [
 //   {
@@ -34,6 +36,29 @@ import DiaryList from "./DiaryList";
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(1);
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+    console.log(res);
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        //emotion은 없기 때문에 랜덤난수를 생성해서 넣어준다
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {

e7e0e3fcd1f22da03eb4cbb084f22dfffb711b8d
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-03-10 23:45:23 +0900

    한 입 크기로 잘라먹는 리액트
    7-4~7-5

--- 한입React/emotion-diary/src/App.js
+++ 한입React/emotion-diary/src/App.js
@@ -1,3 +1,4 @@
+import React, { useReducer, useRef } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -5,20 +6,82 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import RouteTest from "./components/RouteTest";
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) => {
+        it.id === action.data.id ? { ...action.data } : it;
+      });
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataid = useRef(0);
+  //CREATE
+  const onCreate = (data, content, emotion) => {
+    dispatch({
+      type: "CREaATE",
+      data: {
+        id: dataid.current,
+        date: new Date(data).getTIME(),
+        content,
+        emotion,
+      },
+    });
+    dataid.current += 1;
+  };
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+  //EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h2>App.js</h2>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/:id" element={<Diary />} />
-        </Routes>
-        <RouteTest />
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext value={data}>
+      <DiaryDispatchContext value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+            <RouteTest />
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext>
+    </DiaryStateContext>
   );
 }
 
--- 한입React/emotion-diary/src/pages/Home.js
+++ 한입React/emotion-diary/src/pages/Home.js
@@ -1,9 +1,4 @@
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>이곳은 홈 입니다</p>
-    </div>
-  );
+  return <div></div>;
 };
 export default Home;
